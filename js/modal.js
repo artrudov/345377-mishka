05a4194b28a09f6b7c160d728a0e7fa291357b7c
Artem Rudov <artrudov@gmail.com>
2017-10-30 17:28:56 +0300

    Утоление жаждый трэвиса:)

--- js/modal.js
+++ js/modal.js
@@ -1,5 +1,4 @@
 //  Modal window
-
 var offerLink = document.getElementsByClassName("button--buy");
 
 var cartPopup = document.querySelector(".modal-cart");
@@ -10,18 +9,18 @@ if (cartPopup) {
 for (var i = 0; i < offerLink.length; i++) {
   offerLink[i].preventDefault;
   offerLink[i].num = i;
-  offerLink[i].addEventListener("click", function (evt) {
+  offerLink[i].addEventListener("click", function(evt) {
     evt.preventDefault();
     cartPopup.classList.add("modal-cart--show");
   });
 }
 
-cartClose.addEventListener("click", function (evt) {
+cartClose.addEventListener("click", function(evt) {
   evt.preventDefault();
   cartPopup.classList.remove("modal-cart--show");
 });
 
-window.addEventListener("keydown", function (evt) {
+window.addEventListener("keydown", function(evt) {
   if (evt.keyCode === 27) {
     if (cartPopup.classList.contains("modal-cart--show")) {
       cartPopup.classList.remove("modal-cart--show");
